8a7321ec7f0d564c50f9caf5f6f69cffe30d9c8c
Jyoti <jyotikiran12@gmail.com>
2022-08-04 21:55:15 +0530

    Add login screen

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import Login from './Login.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Login.css';
+export default function Login() {
+  return (
+    <main className="main-container">
+      <h1>Welcome to Dishpoll</h1>
+      <div className="login-container">
+        <h2 className="login-header">Login</h2>
+          <label for="username" className="login-label">Username</label>
+          <input type="text" name="username" id="username" placeholder="Username"/> <br />
+          <label for="password" className="login-label">Password</label>
+          <input type="password" name="password" id="password" placeholder="Password" />
+      </div>
+    </main>
+  )
+}
